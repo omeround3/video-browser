a5ba38dfef28ef8d7e10c68bd19d63d5c7c5fbd9
Omer Lev-Ron <omeround3@gmail.com>
2022-01-31 14:53:10 +0200

    First commit - section 3 video 50

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList.vue';
+
+const API_KEY = 'AIzaSyDom6Tttf4cKO4TAidO-6kxsxGWgX4XBes';
+
+export default {
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList
+    },
+    methods: {
+       onTermChange(searchTerm) {
+           axios.get('https://www.googleapis.com/youtube/v3/search', {
+               params: {
+                   key: API_KEY,
+                   type: 'video',
+                   part: 'snippet',
+                   q: searchTerm
+               }
+           }).then(response => console.log(response))
+       } 
+    }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import * as Vue from 'vue';
+import App from './App';
+
+
+Vue.createApp(App).mount('#app');
